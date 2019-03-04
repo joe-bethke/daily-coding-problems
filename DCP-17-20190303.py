5fbb375e7cbd124c4a3b81c8d4c2de4abb412f61
jbethke <35437985+joe-bethke@users.noreply.github.com>
2019-03-04 10:15:06 -0600

    Add files via upload

---
+++ DCP-17-20190303.py
@@ -0,0 +1,85 @@
+"""
+This problem was asked by Google.
+Suppose we represent our file system by a string in the following manner:
+
+The string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
+dir
+    subdir1
+    subdir2
+        file.ext
+The directory dir contains an empty sub-directory subdir1 and a sub-directory subdir2 containing a file file.ext.
+
+The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" represents:
+dir
+    subdir1
+        file1.ext
+        subsubdir1
+    subdir2
+        subsubdir2
+            file2.ext
+
+The directory dir contains two sub-directories subdir1 and subdir2.
+subdir1 contains a file file1.ext and an empty second-level sub-directory subsubdir1.
+subdir2 contains a second-level sub-directory subsubdir2 containing a file file2.ext.
+
+We are interested in finding the longest (number of characters) absolute path to a file within our file system.
+For example, in the second example above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext",
+and its length is 32 (not including the double quotes).
+Given a string representing the file system in the above format,
+return the length of the longest absolute path to a file in the abstracted file system.
+If there is no file in the system, return 0.
+
+Note:
+The name of a file contains at least a period and an extension.
+The name of a directory or sub-directory will not contain a period.
+"""
+
+
+def get_paths(serialized_dir_structure):
+    if "\n" in serialized_dir_structure:
+        root_end = serialized_dir_structure.find("\n")
+        root = serialized_dir_structure[:root_end]
+    else:
+        raise ValueError("Serialized Dir Structure does not contain the correct format or has no subdirectories.")
+    parent_level = 0
+    splits = serialized_dir_structure.split("\n")
+    paths = list()
+    current_path = [root]
+    for sub in splits[1:]:
+        split_sub = sub.split("\t")
+        sub_item = split_sub[-1]
+        level = len(split_sub) - 1
+        if level - 1 == parent_level:
+            current_path.append(sub_item)
+            parent_level = level
+        elif level == parent_level and "." in sub_item:
+            current_path.append(sub_item)
+            paths.append(current_path)
+            current_path = current_path[:-1]
+        else:
+            paths.append(current_path)
+            current_path = current_path[:-(len(current_path) - level)] + [sub_item]
+            parent_level = len(current_path)
+    paths.append(current_path)
+    return paths
+
+
+def solve_by_length(serialized_dir_structure):
+    return max(("/".join(path) for path in get_paths(serialized_dir_structure)
+                if any("." in sub for sub in path)),
+               key=lambda s: len(s))
+
+
+def solve_by_depth(serialized_dir_structure):
+    return max(("/".join(path) for path in get_paths(serialized_dir_structure)
+                if any("." in sub for sub in path)),
+               key=lambda s: len(s.split("/")))
+
+
+print(solve_by_length("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"))
+print(solve_by_length("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
+print(solve_by_depth("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
+
+
+print(solve_by_length("dir\n\tsubdir1\n\t\tsubsubdir1\n\t\t\tsubsubsubdir1\n\t\t\t\tfile1.txt\n\tmyveryveryveryverylongfilenamethatissupposedtoshowthatthisfunctiondoesnotnecessarilyreturnthedeepestfilebutjustthelongerfilepathevenifthefilenameisincrediblylong.py"))
+print(solve_by_depth("dir\n\tsubdir1\n\t\tsubsubdir1\n\t\t\tsubsubsubdir1\n\t\t\t\tfile1.txt\n\tmyveryveryveryverylongfilenamethatissupposedtoshowthatthisfunctiondoesnotnecessarilyreturnthedeepestfilebutjustthelongerfilepathevenifthefilenameisincrediblylong.py"))
